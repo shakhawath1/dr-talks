871d6eb12075a81a9307dfede27c44731ab0a685
shakhawath <m.shakhawath18@gmail.com>
2022-04-18 09:30:41 +0600

    added login page and authentication

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
-// import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
+
 
 const Register = () => {
     const nameRef = useRef('');
@@ -19,36 +19,32 @@ const Register = () => {
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
-    // const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
 
     const navigateLogin = () => {
         navigate('/login');
     }
 
-    if (loading) {
+    if (loading || updating) {
         return <Loading></Loading>
     }
 
     if (user) {
-        console.log('user', user);
+        // console.log('user', user);
     }
 
     const handleRegister = async (event) => {
         event.preventDefault();
-        // const name = nameRef.current.value;
+        const name = nameRef.current.value;
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
         await createUserWithEmailAndPassword(email, password);
-        // await updateProfile({ displayName: name });
+        await updateProfile({ displayName: name });
         console.log('Updated profile');
         navigate('/home');
     }
-
-
-
-
 
 
     return (
@@ -65,8 +61,7 @@ const Register = () => {
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
-                {/* <label className={agree ? 'ps-2': 'ps-2 text-danger'} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
-                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Genius Car Terms and Conditions</label>
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Dr.TALKS Terms and Conditions</label>
                 <Button variant="primary w-50 mx-auto d-block mb-2" type="submit" disabled={!agree}>
                     Register
                 </Button>
