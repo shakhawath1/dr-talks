d6dd448e531bf285f3e0c791d989366c659ea7e9
shakhawath <m.shakhawath18@gmail.com>
2022-06-06 15:37:57 +0600

    test

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
             <h2 className='mt-5'>Services</h2>
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 my-4" >
                 {
-                    services.map(service => <Service service={service}></Service>)
+                    services.map(service => <Service key={service.id} service={service}></Service>)
                 }
             </div >
         </div>
